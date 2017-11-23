d439e1265fa23894631a1856b1fc524980cee235
Alexander Suleymanov <evosultanstvo@gmail.com>
2017-11-23 08:32:21 +0300

    исправил ошибки Travis

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,8 @@
 'use strict';
 
-window.renderStatistics = function(ctx, names, times){
+window.renderStatistics = function (ctx, names, times) {
 
-  //лучшее время
+  // лучшее время
   var getMaxTime = function () {
     var maxTime = -1;
     for (var i = 0; i < times.length; i++) {
@@ -13,7 +13,7 @@ window.renderStatistics = function(ctx, names, times){
     }
     return maxTime;
   };
-  //сортировка по времени
+  // сортировка по времени
   var sortTimes = function () {
     for (var i = 0; i <= times.length - 2; i++) {
       var minValue = times[i];
@@ -30,13 +30,13 @@ window.renderStatistics = function(ctx, names, times){
   };
 
 
-  //гистограмма
-  var drawStatsGraph = function(){
-    //Высота гистограммы 150px
+  // гистограмма
+  var drawStatsGraph = function () {
+    // Высота гистограммы 150px
     var histogramHeight = 150;
-    //Ширина колонки 40px
+    // Ширина колонки 40px
     var barWidth = 40;
-    //Расстояние между колонками
+    // Расстояние между колонками
     // var proportionalStep = 50;
     var proportionalStep = histogramHeight / (getMaxTime() - 0);
     //
@@ -47,34 +47,34 @@ window.renderStatistics = function(ctx, names, times){
     //
     sortTimes();
     //
-    for (var i = 0; i < times.length; i++){
+    for (var i = 0; i < times.length; i++) {
       var randomBlue = 'rgba(0, 0, 255, ' + Math.random() + ')';
 
-      //размеры колонок(ось Х - слева направо горизонталь)
+      // размеры колонок(ось Х - слева направо горизонталь)
       ctx.fillRect(startX + indent * i, startY, barWidth, -(times[i] * proportionalStep));
-      //цвет колонки участника
+      // цвет колонки участника
       ctx.fillStyle = (names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : randomBlue;
       ctx.fillText(names[i], startX + indent * i, startY + lineHeight);
       ctx.fillText(times[i].toFixed(), startX + indent * i, startY - lineHeight - (times[i] * proportionalStep));
     }
 
   };
-  //облако с тенью
-  var drawStatsCloud = function(){
-    //тень облака начало
-    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';//(полупрозрачный чёрный)
+  // облако с тенью
+  var drawStatsCloud = function () {
+    // тень облака начало
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.7)'; // (полупрозрачный чёрный)
     ctx.shadowOffsetX = 10;
     ctx.shadowOffsetY = 10;
-    //облако
+    // облако
     ctx.fillStyle = 'white';
     ctx.beginPath();
     ctx.fillRect(100, 10, 420, 270);
-    //тень облака конец
+    // тень облака конец
     ctx.shadowOffsetX = 0;
     ctx.shadowOffsetY = 0;
 
   };
-  var drawStatsMessage = function(){
+  var drawStatsMessage = function () {
     ctx.font = '16px PT Mono';
     ctx.fillStyle = 'green';
     ctx.fillText('Ура вы победили!', 125, 40);
